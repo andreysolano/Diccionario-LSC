2d2815e94fddf7d8e0a50bced4c91eadeca8f17a
gamedina0317 <63561567+gamedina0317@users.noreply.github.com>
2020-10-10 20:38:06 -0500

    Add files via upload

---
+++ DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/perfil_Palabra.java
@@ -0,0 +1,15 @@
+package com.example.diccionariolsc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class perfil_Palabra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil__palabra);
+
+    }
+}
