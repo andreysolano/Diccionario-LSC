e12626bd88075a6e7bc23f7b2065e72ffbaa8fc1
AndreySolano <ysolano@unal.edu.co>
2020-10-16 23:30:08 -0500

    Lectura usuario 1ra fase

--- DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/Registro_Usuario.java
+++ DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/Registro_Usuario.java
@@ -3,8 +3,13 @@ package com.example.diccionariolsc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Registro_Usuario extends AppCompatActivity {
+
+    EditText nombreIngresado;
+    EditText contraseñaIngresada;
+    EditText contraseña2Ingresada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
