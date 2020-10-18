57d3ea474ac6740fdbf96ff654190dd4464f5b22
gamedina0317 <63561567+gamedina0317@users.noreply.github.com>
2020-10-18 18:55:04 -0500

    Update MainActivity.java

--- DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/MainActivity.java
+++ DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.diccionariolsc;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.*;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import businessLogic.LectorPalabras;
+import data.Palabra;
+import implementacionesED.ListaPalabras;
 
 public class MainActivity extends AppCompatActivity {
-    public static LectorPalabras Lector=new LectorPalabras("base_palabras.xml");
+    public static ListaPalabras palabras=new ListaPalabras();
     public Button botonInvitado;
     public Button botonRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        parseXML();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,Aprender.class);
+                intent.putExtra("Tipo",true);
                 startActivity(intent);
             }
         });
@@ -35,6 +44,46 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public void parseXML(){
+        XmlPullParserFactory parserF;
+        try{
+            parserF=XmlPullParserFactory.newInstance();
+            XmlPullParser parser2=parserF.newPullParser();
+            InputStream is=getAssets().open("Datos/base_palabras.xml");
+            parser2.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,false);
+            parser2.setInput(is,null);
+            ProcessParsing(parser2);
+        }
+        catch(Exception e){
+            System.out.println("Error en 1");
+        }
+    }
+    private void ProcessParsing(XmlPullParser parser) throws XmlPullParserException, IOException {
+        Palabra nueva=null;
+        int tipoEvento=parser.getEventType();
+        while(tipoEvento!=XmlPullParser.END_DOCUMENT){
+            String etiqueta=null;
+            switch(tipoEvento){
+                case XmlPullParser.START_TAG:
+                    etiqueta=parser.getName();
+                    if(etiqueta.equals("Palabra")){
+                        nueva=new Palabra("","");
+                    }
+                    else if(nueva!=null){
+                        if("id".equals(etiqueta)){
+                            nueva.setId(parser.nextText());
+                        }
+                        else if("contenido".equals(etiqueta)){
+                            nueva.setContenido(parser.nextText());
+                        }
+                    }
+            }
+            tipoEvento=parser.next();
+            palabras.push(nueva);
+            nueva=null;
+        }
+
     }
 
 
