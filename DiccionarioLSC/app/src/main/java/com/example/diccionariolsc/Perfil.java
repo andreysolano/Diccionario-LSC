04794cc956cef8fc8c19785790b211ce5067d6fb
gamedina0317 <63561567+gamedina0317@users.noreply.github.com>
2020-12-03 19:26:39 -0500

    Update Perfil.java

--- DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/Perfil.java
+++ DiccionarioLSC/app/src/main/java/com/example/diccionariolsc/Perfil.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
@@ -28,10 +33,21 @@ public class Perfil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        if(ID != null){
-            descargarPalabras();
-        }
-
+        //if(ID != null){
+           // descargarPalabras();
+        //}
+        String lista[]=new String[10];
+        lista[0]="surprise";
+        lista[1]="Dear Person";
+        lista[2]="I";
+        lista[3]="Bet";
+        lista[7]="Saw";
+        lista[8]="The";
+        lista[9]="Last";
+        prepararLista1(lista);
+        String lista2[]=new String [1];
+        lista2[0]="Palabra del dia";
+        prepararLista2(lista2);
         //Barra inferior de navegación
         BottomNavigationView barraNavegacion = (BottomNavigationView)findViewById(R.id.navigation);
         barraNavegacion.setSelectedItemId(R.id.perfil);
@@ -72,5 +88,36 @@ public class Perfil extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
         });
+    }
+    public void prepararLista1(final String arr[]){
+        ListView listV = (ListView) findViewById(R.id.listaPalabras);
+        listV.setVerticalScrollBarEnabled(true);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        listV.setAdapter(adapter);
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String temporal=arr[position];
+                Toast.makeText(Perfil.this, "Imprime: "+temporal, Toast.LENGTH_SHORT).show();
+                //Intent intento = new Intent(getApplicationContext(), Diccionario.class);
+
+            }
+        });
+
+    }
+    public void prepararLista2(final String arr[]){
+        ListView listV = (ListView) findViewById(R.id.DescubrePalabra);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        listV.setAdapter(adapter);
+        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String temporal=arr[position];
+                Toast.makeText(Perfil.this, "Imprime: "+temporal, Toast.LENGTH_SHORT).show();
+                //Intent intento = new Intent(getApplicationContext(), Diccionario.class);
+
+            }
+        });
+
     }
 }
